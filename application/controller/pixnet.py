3cf85503644307ad77233f8ed08a11b199b563ce
elleryq <elleryq@gmail.com>
2011-05-11 18:40:38 +0800

    Modify albums() to return json.

--- application/controller/pixnet.py
+++ application/controller/pixnet.py
@@ -5,6 +5,19 @@ class PixnetController(BaseController):
     """The Pixnet Controller
     """
     def albums(self):
+        if self.params.has_key( "username" ):
+            self.username=self.params[ "username" ]
+            pixnet=Pixnet()
+#print( pixnet.get_users_elleryq() )
+            result = pixnet.get_album_sets( 
+                    {'user': self.username }
+                    )
+            if result['error']==0:
+                self.render(json=self.to_json(result['sets']))
+                return
+        self.render(json=self.to_json({}))
+
+    def albums_old(self):
         """The default method
         related to templates/pixnet/albums.html
         """
